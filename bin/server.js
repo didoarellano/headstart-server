4def6edbf5670c37887fa1a3f005ef3d70b6bd69
Dido Arellano <dido@semicolonstudios.com>
2014-10-20 20:59:23 +0800

    Require user's package.json properly

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
 var server = require('../lib/server.js');
-var config = require('./package.json').headstartServer || {};
+var config = require('package.json').headstartServer || {};
 server(config);
