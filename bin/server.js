348a4130d14808e6c6c2f313df55c0da37b5ce06
Dido Arellano <dido@semicolonstudios.com>
2015-01-26 16:35:39 +0800

    Use package name if logPrefix !exists

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+var titleCase = require('title-case');
 var server = require('../lib/server.js');
-var config = require('package.json').headstartServer || {};
-server(config);
+var pkg = require('package.json');
+var serverConfig = pkg.headstartServer || {};
+
+if (serverConfig.logPrefix == null && pkg.name != null) {
+    serverConfig.logPrefix = titleCase(pkg.name);
+}
+
+server(serverConfig);
